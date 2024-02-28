e37f026319e6dff3cb9ddb7cf0cadc8bcc6c786d
Christian Grewal <58606181+cgrew77@users.noreply.github.com>
2024-02-27 22:17:44 -0800

    Add files via upload

---
+++ Project-final/Enemy/mediumenemy.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var movement_speed = 50.0
+@onready var sprite = $mediumenemy
+@onready var anim = $AnimationPlayer
+@onready var player = get_tree().get_first_node_in_group("player")
+@export var Health = 20
+
+#added christian
+@onready var loot_base = get_tree().get_first_node_in_group("loot")
+@export var experience = 10
+var exp_coin = preload("res://objects/experience_coin.tscn")
+
+
+func _ready():
+	anim.play("Mwalk")
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
+	if direction.x > 0.1:
+		sprite.flip_h = false
+	elif direction.x < -0.1:
+		sprite.flip_h = true
+
+
+
+func _on_hurt_box_hurt(damage):
+	print("ow im at ")
+	Health -= damage
+	print(Health)
+	if Health <= 0:
+		var new_coin = exp_coin.instantiate()
+		new_coin.global_position = global_position
+		new_coin.experience = experience
+		loot_base.call_deferred("add_child", new_coin)
+		queue_free()
